42564446b8fb92308b04ecd0461588c9a9c7ab41
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-26 16:56:34 +0100

    add newtons

--- main.c
+++ main.c
@@ -6,42 +6,98 @@
 #include <stdbool.h>
 double getInput(const char *prompt);
 bool isScientificNotation(const char *input);
-float findRoot(float (*fpr)(float, float), float a1, float a2, float eps, float y);
+float findBisectionRoot(float (*fpr)(float, float), float a1, float a2, float eps, float y);
+float findNewtonRoot(float (*fpr)(float, float), float a1, float a2, float eps, float y);
 float func1(float x, float y);
 float func2(float x, float y);
-void getInitialGuesses(float y, float a1, float a2, float (*fpr)(float, float));
 int main()
 {
-    int choice;
+    int choice, method;
     float x, y, a1, a2, eps;
     float res;
     float (*funcs[])(float x, float y) = {func1, func2};
-    while (1)
+    int result_ans = 0;
+    char ans[] = "yes";
+    char ans1[4];
+    while (result_ans == 0)
     {
-        printf("Choose equation: \n\t1:  cos(y/x)-2sin(1/x)+1/x = 0 \n\t2:  sin(lnx)-cos(lnx)+y*lnx = 0\n");
-        choice = getInput("Enter your answer(\"1\" or \"2\"): ");
-        if ((choice != 1) && (choice != 2))
+        while (1)
         {
-            printf("Please, choose equation again.\n");
+            printf("Choose equation: \n\t1:  cos(y/x)-2sin(1/x)+1/x = 0 \n\t2:  sin(lnx)-cos(lnx)+y*lnx = 0\n");
+            choice = getInput("Enter your answer(\"1\" or \"2\"): ");
+            if ((choice != 1) && (choice != 2))
+            {
+                printf("Please, choose equation again.\n");
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (1)
+        {
+            method = getInput("\nWhich method do you want to use: \n\t 1:  Bisection \n\t 2:  Newton\nEnter your answer(\"1\" or \"2\"): ");
+            if ((method != 1) && (method != 2))
+            {
+                printf("Please, choose method again.");
+            }
+            else
+            {
+                break;
+            }
+        }
+        y = getInput("Enter y: ");
+        if (method == 1)
+        {
+            do
+            {
+                a1 = getInput("Enter a1: ");
+                a2 = getInput("Enter a2: ");
+            } while ((funcs[choice - 1](a1, y) * funcs[choice - 1](a2, y)) >= 0);
+
+            eps = getInput("Enter accuracy: ");
+
+            res = findBisectionRoot(funcs[choice - 1], a1, a2, eps, y);
+            printf("*********************\n");
+            printf(" Bisection Method \n");
+            printf("*********************\n");
+            printf("Root is: %f\n", res);
         }
         else
+        {
+            a1 = getInput("Enter a1: ");
+            a2 = getInput("Enter a2: ");
+            eps = getInput("Enter accuracy: ");
+            res = findNewtonRoot(funcs[choice - 1], a1, a2, eps, y);
+            printf("*********************\n");
+            printf(" Newton's Method \n");
+            printf("*********************\n");
+            printf("Root is: %f\n", res);
+        }
+        while (1)
+        {
+            printf("If you want to continue, please type \"yes\", if you do not - type \"no\":\n");
+            scanf("%3s", ans1);
+            int c;
+            while ((c = getchar()) != '\n')
+                ;
+            if (strcmp(ans1, "yes") == 0 || strcmp(ans1, "no") == 0)
+            {
+                break;
+            }
+            else
+            {
+                printf("Invalid input.\n");
+            }
+        }
+
+        result_ans = strcmp(ans, ans1);
+        if (result_ans != 0)
         {
             break;
         }
     }
-    y = getInput("Enter y: ");
-
-    do
-    {
-        a1 = getInput("Enter a1: ");
-        a2 = getInput("Enter a2: ");
-    } while ((funcs[choice - 1](a1, y) * funcs[choice - 1](a2, y)) >= 0);
-
-    eps = getInput("Enter accuracy: ");
-
-    res = findRoot(funcs[choice - 1], a1, a2, eps, y);
-
-    printf("\tx = %f\n", res);
+    return 0;
 }
 double getInput(const char *prompt)
 {
@@ -100,7 +156,7 @@ bool isScientificNotation(const char *input)
 
     return (eCount == 1) && (digitsBeforeE > 0) && (digitsAfterE > 0);
 }
-float findRoot(float (*fpr)(float, float), float a1, float a2, float eps, float y)
+float findBisectionRoot(float (*fpr)(float, float), float a1, float a2, float eps, float y)
 {
     float x;
     while (fabs(a1 - a2) > eps)
@@ -111,7 +167,25 @@ float findRoot(float (*fpr)(float, float), float a1, float a2, float eps, float 
 
     return x;
 }
-
+float findNewtonRoot(float (*fpr)(float, float), float a, float b, float eps, float y)
+{
+    float x1 = (*fpr)(b, y);
+    float delta = 1.0;
+    do
+    {
+        float derivative = ((*fpr)(x1 + eps, y) - (*fpr)(x1, y)) / eps;
+        if (derivative == 0)
+        {
+            printf("*********************\n");
+            printf("Mathematical Error");
+            printf("*********************\n");
+            return -1;
+        }
+        delta = (*fpr)(x1, y) / derivative;
+        x1 = x1 - delta;
+    } while (fabs(delta) > eps);
+    return x1;
+}
 float func1(float x, float y)
 {
     return (cos(y / x) - 2 * sin(1.0 / x) + 1.0 / x);
